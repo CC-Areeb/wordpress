151a10772172efff27c13dcbb98dc4927dd22a9b
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-12-10 14:22:28 -0500

    call header .php

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,1 +1,14 @@
-I am the header and I load the head for your site \(^-^)/
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(''); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+        <nav>
+            <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        </nav>
+    </header>
